de77acb18cd12906eb27123fd4d82c50e3b0bd98
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-01-22 18:03:46 +0100

    test

--- src/main/scala/answers/infrastructure/user/UserServiceProxy.scala
+++ src/main/scala/answers/infrastructure/user/UserServiceProxy.scala
@@ -1,10 +1,11 @@
 package answers.infrastructure.user
 
-import cats.data.ReaderT
+import cats.data.{Kleisli, OptionT, ReaderT}
 import app.core.Has
 import app.domain.user.{User, UserService}
+import cats.effect.IO
 
 object UserServiceProxy {
-  def getById(id: String): ReaderT[Option, Has[UserService], User] =
+  def getById(id: String): Kleisli[({type L[a] = OptionT[IO, a]})#L, Has[UserService], User] =
     ReaderT { repo => repo.get.getById(id) }
 }
--- src/main/scala/app/domain/user/UserService.scala
+++ src/main/scala/app/domain/user/UserService.scala
@@ -1,5 +1,8 @@
 package app.domain.user
 
+import cats.data.Kleisli
+import cats.effect.IO
+
 case class UserService(repository: UserRepository) {
-  def getById(id: String): Option[User] = repository.getById(id)
+  def getById(id: String): IO[Option[User]] = IO { repository.getById(id) }
 }
