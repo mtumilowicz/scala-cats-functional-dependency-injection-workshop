e7b2624854cf7941a530874033f29d910c63e568
mtumilowicz <michal.tumilowicz01@gmail.com>
2023-02-18 22:01:18 +0100

    naming

--- src/main/scala/common/Has.scala
+++ src/main/scala/common/Has.scala
@@ -7,12 +7,12 @@ final case class Has[A](map: Map[String, Any])
 
 object Has {
   implicit class HasOps[Self <: Has[_]](self: Self) {
-    def get[A](implicit ev: Self <:< Has[A], classTag: Tag[A]): A =
-      self.map(classTag.toString).asInstanceOf[A]
+    def get[A](implicit ev: Self <:< Has[A], tag: Tag[A]): A =
+      self.map(tag.toString).asInstanceOf[A]
 
     def ++[That <: Has[_]](that: That): Self with That =
       Has(self.map ++ that.map).asInstanceOf[Self with That]
   }
-  def succeed[A](value: A)(implicit classTag: Tag[A]): Has[A] =
-    Has(Map(classTag.toString -> value))
+  def succeed[A](value: A)(implicit tag: Tag[A]): Has[A] =
+    Has(Map(tag.toString -> value))
 }
